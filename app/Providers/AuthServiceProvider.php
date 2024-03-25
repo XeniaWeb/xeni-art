6b3507b59ee0c2b6e05d4f9efff677cdfd6daea2
XeniaWeb <xenia1898@gmail.com>
2024-03-25 12:24:48 +0700

    remove Permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-use App\Models\Permission;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,19 +23,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerGates();
-    }
-
-    protected function registerGates()
-    {
-        try {
-            foreach (Permission::pluck('name') as $permission) {
-                Gate::define($permission, function ($user) use($permission) {
-                    return $user->hasPermission($permission);
-                });
-            }
-        } catch (\Exception $e) {
-            info('registerPermissions(): Database not found or not yet migrated. Ignoring user permissions while booting app.');
-        }
+        //
     }
 }
